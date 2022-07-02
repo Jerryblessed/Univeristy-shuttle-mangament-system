18e1f88774ce4d03cd44d52536761b17019ad61a
Jeremiah Ope <106873857+Jerryblessed@users.noreply.github.com>
2022-07-02 12:08:28 +0100

    Update CUSMS_updated.py

--- CUSMS_updated.py
+++ CUSMS_updated.py
@@ -1,165 +1,43 @@
-#!/usr/bin/env python
-# pylint: disable=C0116,W0613
-# This program is dedicated to the public domain under the CC0 license.
+
+# This program is a copy right software.
 
 """
-Simple Bot to handle '(my_)chat_member' updates.
-Greets new users & keeps track of which chats the bot is in.
+A Bot to handle University shuttle business in Covenant University using natural language processing.
+Covenant University is the university this bot is made for.
+However, the locations can be modified to preferred institution.
+
+Dataset:
+The dataset for this bot is in this source code. Like the destination and the bot responses.
 
 Usage:
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-
-and
-
-First, a few callback functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-
-Usage:
-Example of a bot-user conversation using ConversationHandler.
+Example of a shuttle management bot using different python modules.
 Send /start to initiate the conversation.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 
 """
 
-import logging
-from typing import Tuple, Optional
 
-from telegram import (Update, Chat, ChatMember, ParseMode, ChatMemberUpdated,
-                      ReplyKeyboardMarkup, ReplyKeyboardRemove)
+import logging
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
     Updater,
     CommandHandler,
-    CallbackContext,
     MessageHandler,
     Filters,
     ConversationHandler,
-    ChatMemberHandler,
+    CallbackContext,
 )
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
-
-
-
-def extract_status_change(
-        chat_member_update: ChatMemberUpdated,
-) -> Optional[Tuple[bool, bool]]:
-    """Takes a ChatMemberUpdated instance and extracts whether the 'old_chat_member' was a member
-    of the chat and whether the 'new_chat_member' is a member of the chat. Returns None, if
-    the status didn't change.
-    """
-    status_change = chat_member_update.difference().get("status")
-    old_is_member, new_is_member = chat_member_update.difference().get("is_member", (None, None))
-
-    if status_change is None:
-        return None
-
-    old_status, new_status = status_change
-    was_member = (
-            old_status
-            in [
-                ChatMember.MEMBER,
-                ChatMember.CREATOR,
-                ChatMember.ADMINISTRATOR,
-            ]
-            or (old_status == ChatMember.RESTRICTED and old_is_member is True)
-    )
-    is_member = (
-            new_status
-            in [
-                ChatMember.MEMBER,
-                ChatMember.CREATOR,
-                ChatMember.ADMINISTRATOR,
-            ]
-            or (new_status == ChatMember.RESTRICTED and new_is_member is True)
-    )
-
-    return was_member, is_member
-
-
-def track_chats(update: Update, context: CallbackContext) -> None:
-    """Tracks the chats the bot is in."""
-    result = extract_status_change(update.my_chat_member)
-    if result is None:
-        return
-    was_member, is_member = result
-
-    # Let's check who is responsible for the change
-    cause_name = update.effective_user.full_name
-
-    # Handle chat types differently:
-    chat = update.effective_chat
-    if chat.type == Chat.PRIVATE:
-        if not was_member and is_member:
-            logger.info("%s started the bot", cause_name)
-            context.bot_data.setdefault("user_ids", set()).add(chat.id)
-        elif was_member and not is_member:
-            logger.info("%s blocked the bot", cause_name)
-            context.bot_data.setdefault("user_ids", set()).discard(chat.id)
-    elif chat.type in [Chat.GROUP, Chat.SUPERGROUP]:
-        if not was_member and is_member:
-            logger.info("%s added the bot to the group %s", cause_name, chat.title)
-            context.bot_data.setdefault("group_ids", set()).add(chat.id)
-        elif was_member and not is_member:
-            logger.info("%s removed the bot from the group %s", cause_name, chat.title)
-            context.bot_data.setdefault("group_ids", set()).discard(chat.id)
-    else:
-        if not was_member and is_member:
-            logger.info("%s added the bot to the channel %s", cause_name, chat.title)
-            context.bot_data.setdefault("channel_ids", set()).add(chat.id)
-        elif was_member and not is_member:
-            logger.info("%s removed the bot from the channel %s", cause_name, chat.title)
-            context.bot_data.setdefault("channel_ids", set()).discard(chat.id)
-
-
-def show_chats(update: Update, context: CallbackContext) -> None:
-    """Shows which chats the bot is in"""
-    user_ids = ", ".join(str(uid) for uid in context.bot_data.setdefault("user_ids", set()))
-    group_ids = ", ".join(str(gid) for gid in context.bot_data.setdefault("group_ids", set()))
-    channel_ids = ", ".join(str(cid) for cid in context.bot_data.setdefault("channel_ids", set()))
-    text = (
-        f"@{context.bot.username} is currently in a conversation with the user IDs {user_ids}."
-        f" Moreover it is a member of the groups with IDs {group_ids} "
-        f"and administrator in the channels with IDs {channel_ids}."
-    )
-    update.effective_message.reply_text(text)
-
-
-def greet_chat_members(update: Update, context: CallbackContext) -> None:
-    """Greets new users in chats and announces when someone leaves"""
-    result = extract_status_change(update.chat_member)
-    if result is None:
-        return
-
-    was_member, is_member = result
-    cause_name = update.chat_member.from_user.mention_html()
-    member_name = update.chat_member.new_chat_member.user.mention_html()
-
-    if not was_member and is_member:
-        update.effective_chat.send_message(
-            f"{member_name} was added by {cause_name}. Welcome!",
-            parse_mode=ParseMode.HTML,
-        )
-    elif was_member and not is_member:
-        update.effective_chat.send_message(
-            f"{member_name} is no longer with us. Thanks a lot, {cause_name} ...",
-            parse_mode=ParseMode.HTML,
-        )
-
-
-"""""
-Begining of code 2
-"""""
-
+# GENDER, PHOTO, LOCATION, BIO = range(4)
 
 GENDER, DESTINATION, LOCATION, BIO = range(4)
 
@@ -261,13 +139,13 @@ def cancel(update: Update, context: CallbackContext) -> int:
 
 
 def main() -> None:
-    """Start the bot."""
+    """Run the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("5244306869:AAFrJqdmrIpAbKx_HuSWBlhhYVsJEeIuShI")
+    updater = Updater("")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-    # for code 2 it starts here
+
     # Add conversation handler with the states GENDER, DESTINATION, LOCATION and BIO
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
@@ -288,19 +166,6 @@ def main() -> None:
 
     # Start the Bot
     updater.start_polling()
-    # for code 2, it ends here
-
-    # Keep track of which chats the bot is in
-    dispatcher.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
-    dispatcher.add_handler(CommandHandler("show_chats", show_chats))
-
-    # Handle members joining/leaving chats.
-    dispatcher.add_handler(ChatMemberHandler(greet_chat_members, ChatMemberHandler.CHAT_MEMBER))
-
-    # Start the Bot
-    # We pass 'allowed_updates' handle *all* updates including `chat_member` updates
-    # To reset this, simply pass `allowed_updates=[]`
-    updater.start_polling(allowed_updates=Update.ALL_TYPES)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
@@ -308,5 +173,5 @@ def main() -> None:
     updater.idle()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
